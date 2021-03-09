76d194049f621db9fa9d1cf0735b727429818476
nely de faria <nelly.nf54@gmail.com>
2021-03-08 21:29:53 -0300

    Adiciona classe Usuario

---
+++ gerenciador-cursos/src/Entity/Usuario.php
@@ -0,0 +1,34 @@
+<?php
+namespace Alura\Cursos\Entity;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+/**
+ * @Entity
+ * @Table(name="usuarios")
+ */
+class Usuario
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private $id;
+    /**
+     * @Column(type="string")
+     */
+    private $email;
+    /**
+     * @Column(type="string")
+     */
+    private $senha;
+
+    public function senhaEstaCorreta(string $senhaPura): bool
+    {
+        return password_verify($senhaPura, $this->senha);
+    }
+}
