1f62136f4efa3a9bf73a8da170df458708e22bf3
nely de faria <nelly.nf54@gmail.com>
2021-03-14 19:39:59 -0300

    Cria nova rota

--- psrs/config/routes.php
+++ psrs/config/routes.php
@@ -1,8 +1,10 @@
 <?php
 
+use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioInsercao;
 
 return [
     '/novo-curso' => FormularioInsercao::class,
+    '/excluir-curso' => Exclusao::class
 ];
 
