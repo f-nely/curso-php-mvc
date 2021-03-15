c71a6babf690412c3d799df9c043f53f7197b78e
nely de faria <nelly.nf54@gmail.com>
2021-03-14 21:09:14 -0300

    Implementando interface

--- gerenciador-cursos/src/Controller/RealizarLogin.php
+++ gerenciador-cursos/src/Controller/RealizarLogin.php
@@ -5,8 +5,13 @@ namespace Alura\Cursos\Controller;
 use Alura\Cursos\Entity\Usuario;
 use Alura\Cursos\Helper\FlashMessegeTrait;
 use Alura\Cursos\Infra\EntityManagerCreator;
+use Doctrine\ORM\EntityManagerInterface;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class RealizarLogin implements InterfaceControladorRequisicao
+class RealizarLogin implements RequestHandlerInterface
 {
     use FlashMessegeTrait;
 
@@ -15,25 +20,23 @@ class RealizarLogin implements InterfaceControladorRequisicao
      */
     private $repositorioDeUsuarios;
 
-    public function __construct()
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $entityManager = (new EntityManagerCreator())->getEntityManager();
         $this->repositorioDeUsuarios = $entityManager
             ->getRepository(Usuario::class);
     }
 
-    public function processaRequisicao(): void
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $email = filter_input(
-            INPUT_POST,
-            'email',
+        $email = filter_var(
+            $request->getParsedBody()['email'],
             FILTER_VALIDATE_EMAIL
         );
 
+        $redirecionamentoLogin = new Response(302, ['Location' => '/login']);
         if (is_null($email) || $email === false) {
             $this->defineMenssagem('danger', 'O e-mail digitado não é um e-mail válido.');
-            header('Location: /login');
-            return;
+            return $redirecionamentoLogin;
         }
 
         $senha = filter_input(
@@ -50,12 +53,12 @@ class RealizarLogin implements InterfaceControladorRequisicao
         if (is_null($usuario) || !$usuario->senhaEstaCorreta($senha)) {
             $this->defineMenssagem('danger', 'E-mail ou senha inválidos');
             header('Location: /login');
-            return;
+            return $redirecionamentoLogin;
         }
 
         $_SESSION['logado'] = true;
 
-        header('Location: /listar-cursos');
+        return new Response(302, ['Location' => '/listar-cursos']);
     }
 }
 
